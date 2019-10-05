c2d3efbfada0a525c9777f8dba08c50cc1cd8a13
NarnSaper <43374987+NarnSaper@users.noreply.github.com>
2019-10-05 17:28:02 +0300

    Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Stack<string> Bands = new Stack<string>();
+         Bands.Push("Gorgoroth");
+         Bands.Push("Sodom");
+         Bands.Push("Annihilator");
+         Bands.Push("S.O.A.D");
+         
+         List<string> myList = Bands.ToList();
+         foreach (string bands in myList)
+         {
+            Console.WriteLine(bands);
+            if (bands == myList[myList.Count - 1])
+            {
+               Console.WriteLine($"The last element : {bands}");
+            }
+         }
+      }
+   }
+}
